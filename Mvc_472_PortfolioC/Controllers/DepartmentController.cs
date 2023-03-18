9d33c2843515437694a897a83c8b8d28a0d3118a
jmathias3 <john.mathias3@gmail.com>
2023-03-18 08:22:27 -0400

    initial commit

---
+++ Mvc_472_PortfolioC/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_472_PortfolioC.Models;
+
+namespace Mvc_472_PortfolioC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+             
+            List<Department> departments = employeeContext.Departments.ToList();
+            return View(departments);
+        }
+    }
+}
---
+++ Mvc_472_PortfolioC/Controllers/EmployeeController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_472_PortfolioC.Models;
+
+namespace Mvc_472_PortfolioC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index(int? departmentId)
+        {
+            List<Employee> employee;
+
+            EmployeeContext employeeContext = new EmployeeContext();
+
+            if(departmentId != null)
+            {
+                employee = employeeContext.Employees.Where(e => e.DepartmentID == departmentId).ToList();
+            }
+            else
+            {
+                employee = employeeContext.Employees.ToList();
+            }
+            
+
+
+            //Employee Employee = new Employee()
+            //{
+            //    EmployeeId = 101,
+            //    Name = "John",
+            //    Gender = "Male",
+            //    City = "London"
+            //};
+
+            return View(employee);
+        }
+
+        //public ActionResult Index(int departmentId)
+        //{
+        //    EmployeeContext employeeContext = new EmployeeContext();
+        //    List<Employee> employee = employeeContext.Employees.Where(e => e.DepartmentID == departmentId).ToList();
+
+
+        //    //Employee Employee = new Employee()
+        //    //{
+        //    //    EmployeeId = 101,
+        //    //    Name = "John",
+        //    //    Gender = "Male",
+        //    //    City = "London"
+        //    //};
+
+        //    return View(employee);
+        //}
+
+        // GET: Employee
+
+        public ActionResult Details(int id)
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            Employee employee = employeeContext.Employees.Where(x=> x.EmployeeId == id).FirstOrDefault();
+
+
+            //Employee Employee = new Employee()
+            //{
+            //    EmployeeId = 101,
+            //    Name = "John",
+            //    Gender = "Male",
+            //    City = "London"
+            //};
+
+            return View(employee);
+        }
+    }
+}
