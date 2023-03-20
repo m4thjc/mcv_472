7abd0f8b2cb93319f1cb638dd18b8b62eae663c2
jmathias3 <john.mathias3@gmail.com>
2023-03-20 16:40:03 -0400

    customized the entityFramework version of create employee, using partial class and metadata attribute https://learn.microsoft.com/en-us/dotnet/api/system.componentmodel.dataannotations.metadatatypeattribute?view=net-7.0  https://youtu.be/vIxx_B7a8VU

---
+++ Mvc_472_PortfolioC/Models/EfCustEmployee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_472_PortfolioC.Models
+{
+    [MetadataType(typeof(EfEmployeeMetaData))]
+    public partial class EfEmployee
+    {
+    }
+
+    public class EfEmployeeMetaData
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "Department")]
+        public int DepartmentId { get; set; }
+    }
+}
