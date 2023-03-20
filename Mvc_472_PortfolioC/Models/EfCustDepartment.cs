7373f605ef8a173f8b9454193f3619bedf217a59
jmathias3 <john.mathias3@gmail.com>
2023-03-20 12:05:48 -0400

    added a patial efDepartment class that added metadata so we con set name of properties from autogenerated version of EfDepartment https://youtu.be/Y45YpWt4twM

---
+++ Mvc_472_PortfolioC/Models/EfCustDepartment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_472_PortfolioC.Models
+{
+    [MetadataType(typeof(EfCustDepartmentMetaData))]
+    public partial class EfDepartment
+    {
+
+    }
+    public class EfCustDepartmentMetaData
+    {
+        [Display(Name = "Department Name")]
+        public string Name { get; set; }
+    }
+}
