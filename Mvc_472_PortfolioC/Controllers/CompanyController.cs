25f412484df21afb980a54cb62df7fbd82a036e1
jmathias3 <john.mathias3@gmail.com>
2023-03-24 20:26:46 -0400

    Video 36 textbox vs textboxFor strongly typed views https://youtu.be/peKEMGxz3hs?t=600

---
+++ Mvc_472_PortfolioC/Controllers/CompanyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_472_PortfolioC.Models;
+
+namespace Mvc_472_PortfolioC.Controllers
+{
+    public class CompanyController : Controller
+    {
+        // GET: Company
+        public ActionResult Index()
+        {
+            var company = new Company("JohnsCompany");
+            ViewBag.Departments = new SelectList(company.Departments, "Id", "Name");
+            ViewBag.CompanyName = company.CompanyName;
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            var company = new Company("JohnsCompany");
+            return View(company);
+        }
+    }
+}
---
+++ Mvc_472_PortfolioC/Models/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_472_PortfolioC.Models
+{
+    public class Company
+    {
+        private string _name;
+        public Company(string name)
+        {
+            this._name = name;
+        }
+
+        public List<Department> Departments
+        {
+            get
+            {
+                EmployeeContext db = new EmployeeContext();
+                return db.Departments.ToList();
+            }
+        }
+
+        public string CompanyName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
