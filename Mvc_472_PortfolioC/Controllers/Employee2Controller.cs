09cc898e34f45603aa49ae9d1bc1fae51752ba79
jmathias3 <john.mathias3@gmail.com>
2023-03-26 18:02:19 -0400

    Video 40 display attributes

---
+++ Mvc_472_PortfolioC/Controllers/Employee2Controller.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_472_PortfolioC.Models;
+
+namespace Mvc_472_PortfolioC.Controllers
+{
+    public class Employee2Controller : Controller
+    {
+        // GET: Employee2
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Employee2/Details/5
+        public ActionResult Details(int Id)
+        {
+            Sample2 dbContext = new Sample2();
+            var employee = dbContext.Employee2.Single(x => x.Id == Id);
+            return View(employee);
+        }
+
+        // GET: Employee2/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee2/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee2/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee2/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee2/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee2/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Mvc_472_PortfolioC/Models/CustEmployee2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+namespace Mvc_472_PortfolioC.Models
+{
+    [MetadataType(typeof(Employee2MetaData))]
+    public partial class Employee2
+    {
+        
+    }
+    public class Employee2MetaData
+    {
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yy}")]
+        public DateTime HireDate { get; set; }
+
+        [DisplayFormat(NullDisplayText ="Gender not specifed")]
+        public string Gender { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int? Salary { get; set; }
+    }
+
+}
