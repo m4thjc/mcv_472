fd4cef18e9b30e76dda10a57e1214e47f2c307cf
jmathias3 <john.mathias3@gmail.com>
2023-04-10 20:56:34 -0400

    ubtrussive mouseover and mouse out on submit button

---
+++ Mvc_472_PortfolioC/Scripts/CustomJavascript.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="jquery-3.4.1.js" />
+$(function () {
+    $('#btnSubmit').mouseover(function () {
+        $('#btnSubmit').css("backgroundColor", "red");
+    });
+
+    $('#btnSubmit').mouseout(function () {
+        $('#btnSubmit').css("backgroundColor", "blue");
+    });
+
+});
